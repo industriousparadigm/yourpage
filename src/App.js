ea889cf00a411b41330253dabd89c9daab3b4a80
mrsalvini <dsgmcosta@gmail.com>
2019-09-14 17:30:00 +0200

    info kinda displayed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+import face from './img/mariana.jpg'
+import bgImg from './img/kyndall-ramirez-m5Lx_VVCst0-unsplash.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="bg"></div>
+      <div className="App">
+        <section className="text">
+          <p className="greeting">Je m'appelle Mariana!</p>
+          <p className="profession">Media & Development Professional</p>
+          <hr />
+          <p className="description">
+            I spend my time refining ideas to convey my clients' messages and also mothering my little baby who is undoubtedly my muse.
+          </p>
+          <button>Talk to me</button>
+        </section>
+        <section className="photo">
+          <img src={face} alt="Mariana face" />
+        </section>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
